f12a43523ac1a3d9d22ee8726392f7c4ccf0c75c
Grom S <grom.srg@gmail.com>
2012-03-16 11:19:33 +0200

    added 'stop on hover' feature

--- jquery.rotator.js
+++ jquery.rotator.js
@@ -11,29 +11,41 @@
 
         var current_index = 0;
 
+        function rotate(blocks) {
 
+            var current = blocks.first();
+
+            // run the rotateQuotes function according to display_duration
+            return window.setInterval(
+                function() {
+                    current.fadeOut(opts.outSpeed);
+                    current.detach().appendTo(blocks.parent());
+                    current = blocks.parent().children().first();
+                    current.delay(opts.animationDelay).fadeIn(opts.inSpeed);
+                },
+                opts.displayDuration
+            );
+
+        }
 
         return this.each(function() {
 
-            var blocks = $(this).children();
+            var blocks = $(this).children(), intervalId;
 
             if (blocks.size() > 1) {
                 blocks.first().show();        // make sure the first one is shown
                 blocks.not(':first').hide();  // make sure the rest are hidden
-
-                var current = blocks.first();
-
-                // run the rotateQuotes function according to display_duration
-                window.setInterval(
-                    function() {
-                        current.fadeOut(opts.outSpeed);
-                        current.detach().appendTo(blocks.parent());
-                        current = blocks.parent().children().first();
-                        current.delay(opts.animationDelay).fadeIn(opts.inSpeed);
-                    },
-                    opts.displayDuration
-                );
+                intervalId = rotate(blocks);
             } // end if
+
+
+            if (opts.stopOnHover) {
+                blocks.hover(function() {
+                    clearInterval(intervalId);
+                }, function() {
+                    intervalId = rotate(blocks);
+                });
+            }
 
         });
     };
@@ -42,7 +54,8 @@
         outSpeed: 500,          // 0.5 seconds, the speed at which the "hide" animation runs
         inSpeed: 500,           // 0.5 seconds, the speed at which the "show" animation runs
         displayDuration: 5000,  // 5 seconds, how long the item will be displayed
-        animationDelay: 600     // 0.6 seconds, the delay between the "hide" and "show" animation events
+        animationDelay: 600,    // 0.6 seconds, the delay between the "hide" and "show" animation events
+        stopOnHover: true       // Stops rotation on hover
     };
 
 })(jQuery);
